9cb3c84264912d17ee21a5a4014121ec481c9159
dreamage <autoemail@163.com>
2020-11-25 15:17:10 +0800

    Update monitor_by.sh

--- shell/monitor_by.sh
+++ shell/monitor_by.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -d "code=BYXFoffice&key=22579576" https://lijia.sinaapp.com/monitor/report.php   
 #*/1 * * * * /home/monitor.sh >/dev/null 2>&1
-*/1 * * * * cd /home && wget https://cdn.jsdelivr.net/gh/dreamage/l_public@master/shell/monitor_by.sh -O monitor_by.sh && chmod +x /home/monitor_by.sh && /home/monitor_by.sh >/dev/null 2>&1
+#*/1 * * * * cd /home && wget https://cdn.jsdelivr.net/gh/dreamage/l_public@master/shell/monitor_by.sh -O monitor_by.sh && chmod +x /home/monitor_by.sh && /home/monitor_by.sh >/dev/null 2>&1

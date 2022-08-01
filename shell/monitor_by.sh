f1fe3a1e13d7d19e9df0556d19e2d55be6eb9661
dreamage <autoemail@163.com>
2022-08-01 09:11:21 +0800

    Update monitor_by.sh

--- shell/monitor_by.sh
+++ shell/monitor_by.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-curl -d "code=BYXFoffice&key=22579576" https://lijia.sinaapp.com/monitor/report.php   
-#*/1 * * * * /home/monitor.sh >/dev/null 2>&1
-#*/1 * * * * cd /home && wget https://cdn.jsdelivr.net/gh/dreamage/l_public@master/shell/monitor_by.sh -O monitor_by.sh && chmod +x /home/monitor_by.sh && /home/monitor_by.sh >/dev/null 2>&1
-#curl -d '{"code": "BYXFoffice", "key":"22579576"}' https://service-pzn5rmdp-1251116189.bj.apigw.tencentcs.com/monitor_report
+curl -m 10 --retry 5 https://hc-ping.com/c7bf8763-aa7a-4564-a1d9-3a05ca287380
+#*/1 * * * * /home/monitor_by.sh >/dev/null 2>&1
